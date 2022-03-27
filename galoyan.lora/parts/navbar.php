930e67e3b0cb6d1db059ac11785b8a6cb515a0f0
loragaloyan <lkgaloyan@gmail.com>
2022-03-26 18:30:22 -0700

    new pages

---
+++ galoyan.lora/parts/navbar.php
@@ -0,0 +1,16 @@
+	
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Azure Terrariums</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav-flex flex-none">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="product_list.php">Shop</a></li>
+					<li><a href="#">Artist</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
