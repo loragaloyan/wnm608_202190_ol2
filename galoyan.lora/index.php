b295b2ad8940a71c94803426e6b112715cc9556d
loragaloyan <lkgaloyan@gmail.com>
2022-04-10 17:28:28 -0700

    images

---
+++ galoyan.lora/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Azure Terrariums</title> 
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+
+	<?php include "parts/navbar.php"; ?>
+</head>
+
+<body>
+
+	<div class="container body">
+			<h1 class="home">Home</h1>
+			<div class="article_body">
+				<p>Azure Terrarriums are a touch of fantasy decoration for your home, perfect for desks and bookshelves. All components are hand-crafted in unique or custom styles featuring a variety of biomes, cryptids, and containers. </p>
+			</div>
+
+
+		</div>
+	</div>
+
+<div class="container">
+		
+		<div class="items">
+			<div class="item">
+				<img src='img/t7.jpg' class="item-image">
+			</div>
+			<div class="item">
+				<img src='img/t8.jpg' class="item-image">
+			</div>
+			<div class="item">
+				<img src='img/t9.jpg' class="item-image">
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
--- galoyan.lora/parts/navbar.php
+++ galoyan.lora/parts/navbar.php
@@ -1,16 +1,39 @@
-	
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Azure Terrariums</title> 
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	<link rel="stylesheet" href="https://use.typekit.net/guv2awx.css">
+
+ </head>
+
+
+
+<body>
 	<header class="navbar">
-		<div class="container display-flex">
-			<div class="flex-none">
-				<h1>Azure Terrariums</h1>
-			</div>
+		<div class="view-window" style="background-image:url('img/butterflygrey.jpg')">
+		<div class="title">
+			<h1>Azure Terrariums</h1>
+		</div>
+		<div class="menubar">
 			<div class="flex-stretch"></div>
-			<nav class="nav-flex flex-none">
-				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li><a href="product_list.php">Shop</a></li>
-					<li><a href="#">Artist</a></li>
+			<nav class="flex-none nav">
+				<ul class="menu">
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="shop.php">SHOP</a></li>
+					<li><a href="artist.php">ARTIST</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
+
+
+
+</body>
+
