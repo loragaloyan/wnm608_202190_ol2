b4c0b67635349203e931094ce92d7abaee314bdc
loragaloyan <lkgaloyan@gmail.com>
2022-04-16 14:28:18 -0700

    lora

--- galoyan.lora/artist.php
+++ galoyan.lora/artist.php
@@ -4,7 +4,35 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Artist</title> 
+	<title>Azure Terrariums</title> 
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
 
 	<?php include "parts/navbar.php"; ?>
 </head>
+
+<body>
+
+	<div class="container body">
+			<h1 class="home">Lora</h1>
+			<div class="article_body">
+				<p>Azure Terrarriums are a touch of fantasy decoration for your home, perfect for desks and bookshelves. All components are hand-crafted in unique or custom styles featuring a variety of biomes, cryptids, and containers. </p>
+			</div>
+
+
+		</div>
+	</div>
+
+<div class="container">
+		
+		<div class="items">
+			<div class="item">
+				<img src='img/IMG_6523.jpg' class="item-image">
+			</div>
+	</div>
+
+
+</body>
+</html>
